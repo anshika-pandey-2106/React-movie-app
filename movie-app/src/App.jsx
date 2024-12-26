39685a79fbd17261216a25b9681a99101c2d4f80
Anshika Pandey <anshika.p2106@gmail.com>
2024-12-26 22:49:24 +0530

    completed

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -3,23 +3,19 @@ import "./App.css";
 import SearchIcon from "./search.svg";
 import MovieCard from "./MovieCard";
 const API_URL = " http://www.omdbapi.com/?apikey=a3c5a0b5";
-const movie1 = {
-  Title: "Interstellar",
-  Year: "2014",
-  imdbID: "tt0816692",
-  Type: "movie",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BYzdjMDAxZGItMjI2My00ODA1LTlkNzItOWFjMDU5ZDJlYWY3XkEyXkFqcGc@._V1_SX300.jpg",
-};
+
 //api key:a3c5a0b5
 
 const App = () => {
+  
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm]= useState(" ");
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data);
+    
+  setMovies(data.Search);
   };
-  const [] = useState([]);
 
   useEffect(() => {
     searchMovies("interstellar");
@@ -32,15 +28,30 @@ const App = () => {
       <div className="search">
         <input
           placeholder="search for movies"
-          value="superman"
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         ></input>
-        <img src={SearchIcon} alt="search" onClick={() => {}}></img>
+        <img src={SearchIcon} alt="search" onClick={() => searchMovies(searchTerm)}></img>
       </div>
-      <div className="container">
-       <MovieCard movie1={movie1} />
-      </div>
+      
+        {
+          movies.length>0
+          ? (
+            <div className="container">
+                   {movies.map((movie)=> (
+                     <MovieCard movie={movie} />)
+                   )}
+                  </div>):(
+                    <div className="empty">
+                      <h2>No movies found</h2>
+                    </div>
+                  )
+        }
+
+      
+      
     </div>
   );
+
 };
 export default App;
